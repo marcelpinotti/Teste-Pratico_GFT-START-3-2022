c2ab4388b1714f9d64bb92ca3229dbfca8ca1315
marcelpinotti <marcel.pinotti@gmail.com>
2022-02-02 19:30:00 -0300

    Exercicio 4 - Refatorando, adicionando regex e lógica para String vazia

--- src/main/java/fraseInvertida/FraseInvertida.java
+++ src/main/java/fraseInvertida/FraseInvertida.java
@@ -1,5 +1,6 @@
 package fraseInvertida;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class FraseInvertida {
@@ -7,17 +8,21 @@ public class FraseInvertida {
     public static void main (String [] args){
 
         Scanner teclado = new Scanner(System.in);
-        Boolean comparação = true;
+        Boolean comparacao = true;
 
         String frase = teclado.nextLine();
 
-        String fraseInvertida = new StringBuilder(frase).reverse().toString();
+        //adicionando linha de código para aplicar regex
+        String fraseLimpa = frase.toUpperCase(Locale.ROOT).replaceAll("[!?.,:;^[\\\"']+|[\\\"']+$\\S+]", "");
 
-        if (frase.equals(fraseInvertida)){
-            System.out.println(frase  + " -> " +  comparação);
+        String fraseInvertida = new StringBuilder(fraseLimpa).reverse().toString();
+
+        //if (frase.equals(fraseInvertida)){ -> inserindo validação para String vazia
+        if (fraseLimpa.equals(fraseInvertida) && fraseLimpa.isEmpty() == false){
+            System.out.println(frase  + " -> " +  comparacao);
         } else {
-            comparação = false;
-            System.out.println(frase + " -> " + comparação);
+            comparacao = false;
+            System.out.println(frase + " -> " + comparacao);
         }
         teclado.close();
     }
